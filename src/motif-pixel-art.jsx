c014bd92eae52c448b218baab6e96cc65dfef24a
unknown <Dedahisdh@gmail.com>
2025-11-23 14:22:53 +0000

    Quand la souris sort → il reprend sa couleur d’origine.

--- src/motif-pixel-art.jsx
+++ src/motif-pixel-art.jsx
@@ -1,4 +1,5 @@
 import PixelArt from "./pixel-art";
+import PixelArtHover from "./pixel-art-hover";
 import PixelsTab from "./pixels-tab";
 
 export default function MotifPixelArt({ pixels, pixelSize, color }) {
@@ -7,10 +8,11 @@ export default function MotifPixelArt({ pixels, pixelSize, color }) {
       {pixels.map((ligne, ligneIndex) => (
         <div style={{ display: "flex" }} key={ligneIndex}>
           {ligne.map((pixel, pixelIndex) => (
-            <PixelArt
+            <PixelArtHover
               key={pixelIndex}
               size={pixelSize}
-              color={color[pixel] || ""}
+              hoverColor="white"
+              baseColor={color[pixel] || ""}
             />
           ))}
         </div>
